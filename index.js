1262e36c7b957d8c4fb76d92ccd31c6a37aad5f9
shafj-sands <shafik.j@sandsindia.com>
2026-09-19 14:57:43 +0530

    ip changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 
 app.use(cors());
 // Serve static files
